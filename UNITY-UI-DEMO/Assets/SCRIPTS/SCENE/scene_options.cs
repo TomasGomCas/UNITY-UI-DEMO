0a66e10ad62c6587ce877ac745076d1650b038c7
TomasGomCas <38647610+TomasGomCas@users.noreply.github.com>
2021-09-15 18:51:09 +0200

    VERSION-1.0 - LOCATION RESEARCH

--- UNITY-UI-DEMO/Assets/SCRIPTS/SCENE/scene_options.cs
+++ UNITY-UI-DEMO/Assets/SCRIPTS/SCENE/scene_options.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization;
 
 public class scene_options : MonoBehaviour
 {
-    public GameObject gameController;
-    public GameController controller;
+   // public GameObject gameController;
+    // public GameController controller;
 
     public Button button_back;
-    public Slider slider_volume;
+    // public Slider slider_volume;
+    public Dropdown dropdown_language;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameController = GameObject.Find("GameController");
-        this.controller = this.gameController.GetComponent("GameController") as GameController;
-
         this.button_back = GameObject.Find("button_back").GetComponent<Button>();
         this.button_back.onClick.AddListener(back);
 
+        this.dropdown_language = GameObject.Find("Dropdown").GetComponent<Dropdown>();
+        this.dropdown_language.onValueChanged.AddListener(delegate { changeLanguage(); });
+
+        Debug.Log("LENGUAGE ACTUAL // " + LocalizationSettings.SelectedLocale.name);
+
+        if (LocalizationSettings.SelectedLocale.name == "Spanish (es)") {
+            Debug.Log("ESPAGNOL");
+        }
+        else if (LocalizationSettings.SelectedLocale.name == "English (en)") {
+            Debug.Log("INGLE");
+        }
+
+        /*
+        gameController = GameObject.Find("GameController");
+        this.controller = this.gameController.GetComponent("GameController") as GameController;
+
         this.slider_volume = GameObject.Find("slider_volume").GetComponent<Slider>();
         this.slider_volume.value = this.controller.musicController.music_main.volume;
-        this.slider_volume.onValueChanged.AddListener(delegate { volume(); });
+        this.slider_volume.onValueChanged.AddListener(delegate { volume(); });*/
     }
 
     // Update is called once per frame
@@ -32,15 +48,25 @@ public class scene_options : MonoBehaviour
         
         
     }
+
+    void changeLanguage() {
+
+        // 0 - English
+        // 1 - Spanish
+        //this.dropdown_language.value
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[this.dropdown_language.value];
+
+    }
+
     void back()
     {
-        this.controller.musicController.audioClick();
-        SceneManager.LoadScene("scene_mainmenu");
+       // this.controller.musicController.audioClick();
+       // SceneManager.LoadScene("scene_mainmenu");
     }
 
     void volume()
     {
-        this.controller.musicController.music_main.volume = this.slider_volume.value;
+        //this.controller.musicController.music_main.volume = this.slider_volume.value;
     }
 
 }
